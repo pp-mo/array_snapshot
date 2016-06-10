7495290ca6fd9b3ab6442bee7b5bada41ba76e72
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2016-06-10 17:08:54 +0100

    Ideas for flexible strategy controls.

--- array_snapshot.py
+++ array_snapshot.py
@@ -17,8 +17,88 @@ from sample_indices import sample_indices as sample_indices_for_shape
 
 DEFAULT_RELTOL = 1.0e-7
 DEFAULT_ABSTOL = 1.0e-7
+
+
+def points_indices(shape, **kwargs):
+    return sample_indices_for_shape(shape)
+
+
+def decimate(array, decimations=None, min_n_points=50):
+    decimations = kwargs.get('decimations')
+    if decimations:
+        # Slice array to get a decimated version for stats calculations.
+        slices = [slice(None, None, decimate)
+                  for decimate in decimations]
+        slices = slices[:len(array.shape)]
+        n_extra_dims = len(array.shape) - len(slices)
+        if n_extra_dims > 0:
+            # Repeat the last one as required.
+            slices += [slices[-1]] * n_extra_dims
+        array = array[slices]
+        #
+        # TODO: needs more intelligence for small arrays
+        #  - add 'min_n_points' support.
+        #
+    return array
+
+
+def is_categorical_by_value_set(array, max_n_values_tolerable=20):
+    return len(set(array)) > max_n_values_tolerable
+
+
+def add_balanced_dither(array, kernel, normalise=True, range_ratio=0.5):
+    a_min, a_max = np.min(array), np.max(array)
+    kernel = np.asarray(kernel)
+    if normalise:
+        k_min, k_max = np.min(kernel), np.max(kernel)
+        kernel = kernel - 0.5 * (k_min + k_max)
+        kernel = 2.0 / (k_max - k_min)
+    dither = balanced_dither_array_for_shape(array.shape, pattern=dither,
+                                             amplitude=range_ratio)
+    array = array + dither  # Converts to float if not already
+    return array
+
+
+DEFAULT_POINT_SAMPLING = {
+    'method':points_indices,
+    'kwargs':{},
+}
+
+DEFAULT_STATS_SAMPLING = {
+    'method':decimate,
+    'kwargs':{'target_n_samples':24, 'dithers':[10]}
+}
+
+
+DEFAULT_DITHER_USAGE_SAMPLING = {
+    'method':decimate,
+    'kwargs':{'decimations':[50], 'min_n_points':100}
+}
+
+DEFAULT_DITHER_USAGE_TEST = {
+    'method':is_categorical_by_value_set,
+    'kwargs':{'max_n_values_tolerable':25}
+}
+
+DEFAULT_DITHER_OPERATION_SAMPLING = {
+    'method':decimate,
+    'kwargs':{'decimations':[10], 'min_n_points':50}
+}
+
+DEFAULT_DITHER_OPERATION = {
+    'method':add_balanced_dither,
+    'kwargs':{'kernel':DEFAULT_DITHER_KERNEL,
+              'normalise':True,
+              'range_ratio':0.5}
+}
+
 DEFAULT_MAX_CATEGORIES = 25
 DEFAULT_STATS_DECIMATIONS = []
+
+def control_method_and_kwargs(spec):
+    method, kwargs = (spec.get(name) for name in ('method', 'kwargs'))
+    return method, kwargs
+
 
 class ArraySnapshot(object):
     default_reltol = DEFAULT_RELTOL
@@ -71,7 +151,8 @@ class ArraySnapshot(object):
     def _take_snapshot(self, array):
         """"Update to match the given array (plain ndarray, *NOT* masked)."""
         self.shape = array.shape
-        # Record snapshot of the mask, if we have one
+
+        # Snapshot the mask, if we have one.
         if not isinstance(array, np.ma.MaskedArray):
             self.mask = None
             self.fill_value = None
@@ -79,39 +160,46 @@ class ArraySnapshot(object):
             # Treat the mask as a total array, so we can compare snapshots.
             self.mask = ArraySnapshot(np.ma.getmaskarray(array))
             self.fill_value = array.fill_value
-            # Remainder of the snapshot is done on the filled array.
+            # Remainder of the snapshot is done on the *filled* array.
+            #
+            # TODO: work out how to delay this until access *everywhere* ?
+            #  could be a job for biggus ???
+            #
             array = array.filled()
+
         # Form stats, decimating if enabled
-        if not self.stats_decimations:
-            # Use whole array
+        method, kwargs = control_method_and_kwargs(self.stats_sampling)
+        if not method:
             stats_array = array
         else:
-            # Slice array to get a decimated version for stats calculations.
-            slices = [slice(None, None, decimate)
-                      for decimate in self.stats_decimations]
-            slices = slices[:len(array.shape)]
-            stats_array = array[slices]
+            stats_array = method(array, **kwargs)
         self.stats_min = np.min(stats_array)
         self.stats_max = np.max(stats_array)
         self.stats_mean = np.mean(stats_array)
-        self.sample_point_indices = sample_indices_for_shape(self.shape)
+
+        # Snapshot specific points
+        method, kwargs = control_method_and_kwargs(self.point_sampling)
+        self.sample_point_indices = method(array.shape, **kwargs)
         self.sample_point_values = [array[inds]
                                     for inds in self.sample_point_indices]
-        max_treated_as_categories = len(self.sample_point_indices) / 2
-        if self.max_categories < max_treated_as_categories:
-            max_treated_as_categories = self.max_categories
-        self.is_categorical = (len(set(self.sample_point_values)) <
-                               max_treated_as_categories)
+
+        # Record dithered mean if required
+        method, kwargs = control_method_and_kwargs(self.dither_usage_sampling)
+        if method is None:
+            dither_usage_samples = array
+        else:
+            # Reduce input array for dither usage testing.
+            dither_usage_samples = method(array, kwargs)
+        method, kwargs = control_method_and_kwargs(self.dither_usage_test)
+        self.is_categorical = method(dither_usage_samples, **kwargs)
         if not self.is_categorical:
             self.dithered_mean = 0.0
         else:
-            # Calculate and attach a dithered total mean.
-            dither = balanced_dither_array_for_shape(
-                self.shape, pattern=self.dither_kernel)
-            # TODO: or something ...
-            dither = (dither - 0.5) * (self.stats_max - self.stats_min)
-            total_array = array + dither
-            self.dithered_mean = np.mean(total_array)
+            method, kwargs = control_method_and_kwargs(self.dither_sampling)
+            dither_samples = method(array, **kwargs)
+            method, kwargs = control_method_and_kwargs(self.dither_operation)
+            dithered_array = method(dither_samples, **kwargs)
+            self.dithered_mean = np.mean(dithered_array)
 
     @staticmethod
     def from_str(string):
